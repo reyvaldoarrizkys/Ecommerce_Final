05b5f0cd056ea2810b24585f87794ebe96a56093
Reyvaldo Arrizky <user@192.168.1.14>
2023-05-15 19:02:48 +0700

    ecommerce

--- formprosespemesanan.php
+++ formprosespemesanan.php
@@ -1,6 +1,19 @@
 <?php
-require_once "../Ecommerce_Final/admin/dbkoneksi.php";
+$host = 'localhost';
+$db = 'db_reyv22213ti';
+$user = 'reyv22213ti';
+$pass = '19670110222213';
+$charset='utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$opt = [
+  PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES=>false,
+];
+
+$dbh =  new PDO($dsn,$user,$pass,$opt);
 
 // mendapatkan nilai dari form
 $_tanggal = $_POST["tanggal"];
